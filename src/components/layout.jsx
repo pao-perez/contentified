bde1b01e1dff14a1d34d14b5288acd48b80197ef
Pao Perez <pao.pacheco.perez@gmail.com>
2021-04-16 15:26:53 +0800

    Page Structure: Move base structure markup to layout and separate home page

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,5 +1,87 @@
 import * as React from "react";
 
-const Layout = () => <div>Layout</div>;
+const Layout = ({ children }) => (
+  <div className="container">
+    <header>
+      <a href="/" title="Go to home page">
+        <img src="../images/logo.png" alt="Site logo" />
+      </a>
+      <nav aria-label="Primary">
+        <ul>
+          <li>
+            <a href="#">Primary nav</a>
+          </li>
+          <li>
+            <a href="#">Primary nav</a>
+          </li>
+          <li>
+            <a href="#">Primary nav</a>
+          </li>
+          <li>
+            <a href="#">Primary nav</a>
+          </li>
+        </ul>
+      </nav>
+      <form role="search" aria-label="site">
+        <label>
+          <span>Search</span>
+          <input type="search" />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </header>
 
+    <main>{children}</main>
+
+    <aside aria-label="Sidebar">
+      <section>
+        <h2>Share</h2>
+        <ul>
+          <li>
+            <a href="#">Facebook</a>
+          </li>
+          <li>
+            <a href="#">Twitter</a>
+          </li>
+          <li>
+            <a href="#">Email</a>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2>Subscribe</h2>
+        <article>
+          <h3>Get notified every new related content lorem ipsum</h3>
+          <form aria-label="subscribe to newsletter">
+            <label>
+              <span>Subscribe</span>
+              <input type="email" />
+            </label>
+            <button type="submit">Submit</button>
+          </form>
+        </article>
+      </section>
+    </aside>
+
+    <footer>
+      <ul>
+        <li>
+          <a href="#">Footer link</a>
+        </li>
+        <li>
+          <a href="#">Footer link</a>
+        </li>
+        <li>
+          <a href="#">Footer link</a>
+        </li>
+        <li>
+          <a href="#">Footer link</a>
+        </li>
+        <li>
+          <a href="#">Footer link</a>
+        </li>
+      </ul>
+    </footer>
+  </div>
+);
 export default Layout;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,87 +1,33 @@
-import React from "react"
+import React from "react";
 
-import "./index.scss"
+import "./index.scss";
+import Layout from "../components/layout";
 
-const IndexPage = () => {
-  return (
-    <div className="container">
-      <header>
-        <a href="/" title="Go to home page">
-          <img src="logo.png" alt="Site logo" />
-        </a>
-        <nav aria-label="Primary">
-          <ul>
-            <li><a href="#">Primary nav</a></li>
-            <li><a href="#">Primary nav</a></li>
-            <li><a href="#">Primary nav</a></li>
-            <li><a href="#">Primary nav</a></li>
-          </ul>
-        </nav>
-        <form role="search" aria-label="site">
-          <label>
-            <span>Search</span>
-            <input type="search" />
-          </label>
-          <button type="submit">Submit</button>
-        </form>
-      </header>
-
-      <main>
-        <article>
-          <ul>
-            <li>
-              <article>
-                <h1><a href="#">Main article heading</a></h1>
-                <p>Article description</p>
-              </article>
-            </li>
-            <li>
-              <article>
-                <h2><a href="#">Article secondary heading</a></h2>
-                <p>Article description</p>
-              </article>
-            </li>
-          </ul>
-        </article>
-      </main>
-
-      <aside aria-label="Sidebar">
-        <section>
-          <h2>Share</h2>
-          <ul>
-            <li><a href="#">Facebook</a></li>
-            <li><a href="#">Twitter</a></li>
-            <li><a href="#">Email</a></li>
-          </ul>
-        </section>
-        <section>
-          <h2>Subscribe</h2>
+const IndexPage = () => (
+  <Layout>
+    <article>
+      <ul>
+        <li>
           <article>
-            <h3>Get notified every new related content lorem ipsum</h3>
-            <form aria-label="subscribe to newsletter">
-              <label>
-                <span>Subscribe</span>
-                <input type="email" />
-              </label>
-              <button type="submit">Submit</button>
-            </form>
+            <h1>
+              <a href="#">Main article heading</a>
+            </h1>
+            <p>Article description</p>
           </article>
-        </section>
-      </aside>
-
-      <footer>
-        <ul>
-          <li><a href="#">Footer link</a></li>
-          <li><a href="#">Footer link</a></li>
-          <li><a href="#">Footer link</a></li>
-          <li><a href="#">Footer link</a></li>
-          <li><a href="#">Footer link</a></li>
-        </ul>
-      </footer>
-    </div>
-  )
-}
-export default IndexPage
+        </li>
+        <li>
+          <article>
+            <h2>
+              <a href="#">Article secondary heading</a>
+            </h2>
+            <p>Article description</p>
+          </article>
+        </li>
+      </ul>
+    </article>
+  </Layout>
+);
+export default IndexPage;
 
 /**
  * 3. Write all mark up (No styling / logic) in index page
