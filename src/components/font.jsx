65a93ff186469a36d69ecfdaca0889a396d42c64
Pao Perez <pao.pacheco.perez@gmail.com>
2021-06-08 15:04:46 +0800

    Style: Integrate Google Font

---
+++ src/components/font.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Helmet } from 'react-helmet';
+
+const Font = () => (
+  <>
+    <Helmet>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+        rel="stylesheet"
+      />
+    </Helmet>
+  </>
+);
+
+export default Font;
