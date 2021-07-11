46cb780ed4251050c4ba8d9c4ac5a66f00591410
Pao Perez <pao.pacheco.perez@gmail.com>
2021-07-12 05:35:08 +0800

    Feature: implement avatar component

--- src/components/avatar.jsx
+++ src/components/avatar.jsx
@@ -1,15 +1,23 @@
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import './avatar.scss';
 
 const Avatar = ({ avatar, author }) => {
   const image = getImage(avatar);
   return (
     <>
-      <figure>
+      <figure className="avatar">
         <GatsbyImage image={image} alt="Avatar" />
-        <figcaption>{author}</figcaption>
+        <figcaption className="center">{author}</figcaption>
       </figure>
     </>
   );
+};
+
+Avatar.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
 };
 export default Avatar;
