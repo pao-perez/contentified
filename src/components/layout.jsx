e87290e92d593e1470d4f4bf94b067e6b6db29d0
Pao Perez <pao.pacheco.perez@gmail.com>
2021-08-19 17:57:39 +0800

    Style: layout adjustments

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -10,7 +10,7 @@ const Layout = ({ children }) => (
     <Font />
     <div className="container">
       <header className="layout-header">
-        <div className="brand center">
+        <div className="center brand">
           <figure>
             <StaticImage
               src="../images/brand.png"
@@ -21,7 +21,7 @@ const Layout = ({ children }) => (
             />
           </figure>
         </div>
-        <nav className="center" aria-label="Site Navigation">
+        <nav className="center nav" aria-label="Site Navigation">
           <ul>
             <li>
               <Link to="/">Home</Link>
@@ -34,14 +34,14 @@ const Layout = ({ children }) => (
             </li>
           </ul>
         </nav>
-        <form className="center" role="search" aria-label="Search Site">
+        <form className="center search" role="search" aria-label="Search Site">
           <input type="search" placeholder="Search Articles..." />
         </form>
       </header>
 
       <main className="layout-main">{children}</main>
 
-      <footer className="layout-footer center">
+      <footer className="layout-footer">
         <nav aria-label="Other Site Information">
           <ul>
             <li>
@@ -60,8 +60,8 @@ const Layout = ({ children }) => (
               <a href="https://www.google.com/">Footer link</a>
             </li>
           </ul>
-          <p className="center">© 2021. All rights reserved. Contentually</p>
         </nav>
+        <p className="center">© 2021. All rights reserved. Contentually</p>
       </footer>
     </div>
   </>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,53 +6,103 @@ import Layout from '../components/layout';
 
 import './index.scss';
 
-const IndexPage = ({ data }) => (
-  <Layout>
-    <div className="home">
-      <section className="blog-list">
-        <ul>
-          {data.allMarkdownRemark.edges.map(({ node }) => (
-            <li>
-              <Link to={node.fields.slug}>
-                <article className="blog-item">
-                  <header className="item-header">
-                    <h1 className="title">{node.frontmatter.title}</h1>
-                    <h2 className="meta">
-                      <span className="author">{node.frontmatter.author}</span>
-                      <span className="date">
-                        <time dateTime="true">{node.frontmatter.date}</time>
-                      </span>
-                      <span className="tags">
-                        {node.frontmatter.tags.join(', ')}
-                      </span>
-                    </h2>
-                  </header>
-                  <main className="item-main">
-                    <p>{node.excerpt}</p>
-                  </main>
-                  <footer className="item-footer">
-                    <Image
-                      image={node.frontmatter.thumbnail}
-                      alt="Featured Image Thumbnail"
-                      className="thumbnail"
-                    />
-                  </footer>
-                </article>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </section>
-      <aside aria-label="Subscribe to Newsletter" className="newsletter">
-        <EmailNewsletter />
-      </aside>
-    </div>
-  </Layout>
-);
+const IndexPage = ({ data }) => {
+  // sort by multiple fields doesn't work with graphql for some reason, so doing the sort by priority here instead
+  const sortedByPriority = data.allMarkdownRemark.edges.sort((edgeA, edgeB) => {
+    if (edgeA.node.frontmatter.priority < edgeB.node.frontmatter.priority) {
+      return 1;
+    }
+    if (edgeA.node.frontmatter.priority > edgeB.node.frontmatter.priority) {
+      return -1;
+    }
+    return 0;
+  });
+  return (
+    <Layout>
+      <div className="home">
+        <section className="blog-feature">
+test
+        </section>
+        <section className="blog-list">
+          <ul>
+            {sortedByPriority.map(({ node }) => (
+              <li key={node.id}>
+                <Link to={node.fields.slug}>
+                  {node.frontmatter.priority >= 0 && (
+                    <article className="blog-feature">
+                      <header className="item-header">
+                        <Image
+                          image={node.frontmatter.thumbnail}
+                          alt="Featured Image Thumbnail"
+                          className="thumbnail"
+                        />
+                      </header>
+                      <main className="item-main">
+                        <h1 className="title">{node.frontmatter.title}</h1>
+                        <h2 className="meta">
+                          <span className="author">
+                            {node.frontmatter.author}
+                          </span>
+                          <span className="date">
+                            <time dateTime="true">{node.frontmatter.date}</time>
+                          </span>
+                          <span className="tags">
+                            {node.frontmatter.tags.join(', ')}
+                          </span>
+                        </h2>
+                      </main>
+                      <footer className="item-footer">
+                        <p>{node.excerpt}</p>
+                      </footer>
+                    </article>
+                  )}
+                  {node.frontmatter.priority < 0 && (
+                    <article className="blog-item">
+                      <header className="item-header">
+                        <h1 className="title">{node.frontmatter.title}</h1>
+                        <h2 className="meta">
+                          <span className="author">
+                            {node.frontmatter.author}
+                          </span>
+                          <span className="date">
+                            <time dateTime="true">{node.frontmatter.date}</time>
+                          </span>
+                          <span className="tags">
+                            {node.frontmatter.tags.join(', ')}
+                          </span>
+                        </h2>
+                      </header>
+                      <main className="item-main">
+                        <p>{node.excerpt}</p>
+                      </main>
+                      <footer className="item-footer">
+                        <Image
+                          image={node.frontmatter.thumbnail}
+                          alt="Featured Image Thumbnail"
+                          className="thumbnail"
+                        />
+                      </footer>
+                    </article>
+                  )}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </section>
+        <aside aria-label="Subscribe to Newsletter" className="newsletter">
+          <EmailNewsletter />
+        </aside>
+      </div>
+    </Layout>
+  );
+};
 
 export const query = graphql`
   query {
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(
+      limit: 10
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
       totalCount
       edges {
         node {
@@ -62,13 +112,14 @@ export const query = graphql`
             date(formatString: "MMMM DD, YYYY")
             tags
             author
+            priority
             thumbnail {
               childImageSharp {
                 gatsbyImageData(
-                  width: 400
+                  width: 1000
                   placeholder: BLURRED
-                  blurredOptions: { width: 50 }
-                  aspectRatio: 1.75
+                  blurredOptions: { width: 100 }
+                  aspectRatio: 1.5
                 )
               }
             }
