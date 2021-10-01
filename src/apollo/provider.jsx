92a4ba53e0b3ece6cc7411011c7002477d9411c5
Pao Perez <pao.pacheco.perez@gmail.com>
2021-10-01 17:10:15 +0800

    Correct comment

--- src/apollo/provider.jsx
+++ src/apollo/provider.jsx
@@ -4,8 +4,9 @@ import { ApolloProvider } from '@apollo/client';
 
 import client from './client';
 
-// export default const Provider won't let me do:
-// const Provider = require('./ src / apollo / provider');
+// export default const Provider = ({ element }) =>
+// won't let me do:
+// const Provider = require('./src/apollo/provider');
 // in gatsby-browser and gatsby-ssr files for some reason
 export const Provider = ({ element }) => (
   <ApolloProvider client={client}>{element}</ApolloProvider>
