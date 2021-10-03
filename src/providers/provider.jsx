2b4e578468c57e7e8c80b0d10cac3d15f54894d4
Pao Perez <pao.pacheco.perez@gmail.com>
2021-10-03 23:46:19 +0800

    Chore: address lint errors (destructure children props + shadowing var search)

--- src/providers/provider.jsx
+++ src/providers/provider.jsx
@@ -1,27 +1,26 @@
-
 import { ApolloProvider } from '@apollo/client';
 import React, { useState } from 'react';
 import client from '../apollo/client';
 
 export const SearchContext = React.createContext();
 
-const SearchContextProvider = (props) => {
+const SearchContextProvider = ({ children }) => {
   const [search, setSearch] = useState('');
 
   return (
-    <SearchContext.Provider value={{
-      search,
-      setSearch: (search) => setSearch(search)
-    }}>
-      {props.children}
+    <SearchContext.Provider
+      value={{
+        search,
+        setSearch: (searchTerm) => setSearch(searchTerm),
+      }}
+    >
+      {children}
     </SearchContext.Provider>
   );
 };
 
 export const Provider = ({ element }) => (
   <ApolloProvider client={client}>
-    <SearchContextProvider>
-      {element}
-    </SearchContextProvider>
+    <SearchContextProvider>{element}</SearchContextProvider>
   </ApolloProvider>
 );
