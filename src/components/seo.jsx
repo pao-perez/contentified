57a6e087a7d7b5231bc49c4fc0b48e345b50e040
Pao Perez <pao.pacheco.perez@gmail.com>
2021-10-15 04:27:22 +0800

    Fix: revert changes to SEO

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -18,30 +18,66 @@ const SEO = ({ postTitle, postDescription, postUrl, postTags, postAuthor }) => {
   const url = postUrl || siteUrl;
   const keywords = postTags || siteTags;
   const author = postAuthor || siteAuthor.name;
-  const type = `website`;
 
   return (
     <>
       <Helmet
         defer={false} // workaround for when the title doesn’t appear in the tab bar until switching to that tab https://github.com/nfl/react-helmet/issues/315
+        htmlAttributes={{
+          siteLang,
+        }}
+        title={title}
         titleTemplate={`${siteTitle} - %s`}
-      >
-        <html lang={siteLang} />
-        <title>{title}</title>
-        <link rel="canonical" href={url} />
-        <meta name="description" content={description} />
-        {/* meta keywords is ignored by Google but declare it for other Search engines anyway */}
-        <meta name="keywords" content={keywords.join(', ')} />
-
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:url" content={url} />
-        <meta property="og:type" content={type} />
-
-        <meta property="twitter:creator" content={author} />
-        <meta property="twitter:title" content={title} />
-        <meta property="twitter:description" content={description} />
-      </Helmet>
+        link={[
+          {
+            rel: 'canonical',
+            href: url,
+          },
+        ]}
+        meta={[
+          {
+            name: `description`,
+            content: description,
+          },
+          {
+            /* meta keywords is ignored by Google but declare it for other Search engines anyway */
+            name: `keywords`,
+            content: keywords.join(', '),
+          },
+          {
+            property: `og:title`,
+            content: title,
+          },
+          {
+            property: `og:description`,
+            content: description,
+          },
+          {
+            property: `og:url`,
+            content: url,
+          },
+          {
+            property: `og:type`,
+            content: `website`,
+          },
+          {
+            name: `twitter:card`,
+            content: `summary`,
+          },
+          {
+            name: `twitter:creator`,
+            content: author,
+          },
+          {
+            name: `twitter:title`,
+            content: title,
+          },
+          {
+            name: `twitter:description`,
+            content: description,
+          },
+        ]}
+      />
     </>
   );
 };
