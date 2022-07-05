50962d1040f62d8b7c37d4917eca8e7e1a3cc22e
Pao Perez <pao.pacheco.perez@gmail.com>
2022-07-05 16:51:52 +0800

    Migration (styled-components): Move blogpost page to styled components

---
+++ src/components/core/aside.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Aside = ({ className, children }) => (
+    <aside className={className}>
+        {children}
+    </aside>
+);
+
+export default Aside;
--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -1,23 +1,137 @@
+import React, { useContext } from 'react';
 import { graphql } from 'gatsby';
-import React from 'react';
+import styled from 'styled-components';
+import { ThemeContext } from '../providers/theme';
 import Image from '../components/image';
 import SEO from '../components/seo';
 import Share from '../components/share';
 import Tags from '../components/tags';
-import './blog-post.scss';
+import Div from '../components/core/div';
+import Aside from '../components/core/aside';
+import Article from '../components/core/article';
+import Section from '../components/core/section';
+
+const StyledDiv = styled(Div)`
+  margin-top: 2rem;
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: 1fr 2fr 1fr;
+  grid-template-rows: auto;
+  grid-template-areas:
+    "share blog-post .";
+
+  @media (max-width: 1024px) {
+    grid-template-columns: 1fr 9fr;
+  }
+`;
+
+const StyledAside = styled(Aside)`
+  grid-area: share;
+  position: fixed;
+  left: 0.5rem;
+  margin-top: 3rem;
+`;
+
+const StyledArticle = styled(Article)`
+  grid-area: blog-post;
+`;
+
+const StyledHeaderSection = styled(Section)`
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: 1fr 9fr;
+  grid-template-rows: auto;
+  grid-template-areas:
+    "avatar text";
+  font-family: ${props => props.theme.primary.font};
+
+  * {
+    padding: 0rem;
+    margin: 0rem;
+  }
+
+  .avatar {
+    grid-area: avatar;
+    display: flex;
+    align-items: center;
+
+    figure {
+      img {
+        border-radius: 50%;
+      }
+    }
+  }
+
+  .text {
+    grid-area: text;
+    align-self: center;
+
+    .title {
+      font-size: x-large;
+      padding-bottom: .2rem;
+      color: ${props => props.theme.primary.text};
+    }
+
+    .meta {
+      font-weight: lighter;
+      font-size: small;
+
+      .author {
+        font-size: medium;
+        color: ${props => props.theme.primary.text};
+      }
+
+      time {
+        font-style: italic;
+        color: ${props => props.theme.primary.text};
+      }
+    }
+  }
+
+  @media (max-width: 1024px) {
+    grid-template-columns: auto;
+    grid-template-areas:
+      "text";
+
+    .avatar {
+      display: none;
+    }
+
+    .text {
+      .title {
+        font-size: large;
+      }
+
+      .meta {
+        font-size: smaller;
+
+        .author {
+          font-size: small;
+        }
+      }
+    }
+  }
+`;
+
+const StyledBodySection = styled(Section)`
+  font-size: large;
+  font-family: ${props => props.theme.secondary.font};
+  color: ${props => props.theme.secondary.text};
+`; 
 
 const BlogPost = ({ data, location }) => {
   const { title, tags, author, date, avatar } = data.markdownRemark.frontmatter;
   const { html, excerpt } = data.markdownRemark;
   const url = location.href;
+  const { theme } = useContext(ThemeContext);
 
   return (
-    <div className="blog">
-      <aside aria-label="Share on Social Media">
-        <Share title={title} url={url} twitterHandle={author} tags={tags} className="social-media" />
-      </aside>
+    <StyledDiv>
+      <StyledAside aria-label="Share on Social Media">
+        <Share title={title} url={url} twitterHandle={author} tags={tags} />
+      </StyledAside>
 
-      <article className="blog-post">
+      <StyledArticle>
         <SEO
           title={title}
           description={excerpt}
@@ -25,7 +139,7 @@ const BlogPost = ({ data, location }) => {
           tags={tags}
           author={author}
         />
-        <section className="post-header">
+        <StyledHeaderSection theme={theme}>
           <section className="avatar">
             <Image image={avatar} alt="Avatar" className="avatar" />
           </section>
@@ -34,16 +148,16 @@ const BlogPost = ({ data, location }) => {
             <h2 className="meta">
               <span>
                 by <span className="author">{author}</span> on <time dateTime="true">{date}</time> in{" "}
-                <Tags tags={tags} />
+                <Tags tags={tags} theme={theme} />
               </span>
             </h2>
           </section>
-        </section>
-        <section className="post-body">
+        </StyledHeaderSection>
+        <StyledBodySection theme={theme}>
           <p dangerouslySetInnerHTML={{ __html: html }} />
-        </section>
-      </article>
-    </div>
+        </StyledBodySection>
+      </StyledArticle>
+    </StyledDiv>
   );
 };
 
